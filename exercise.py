72b55d9c53c5a36e3c5e1684eee38fd538cfaa8d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-09-27 14:20:55 +0200

    Add axes labels

--- exercise.py
+++ exercise.py
@@ -4,5 +4,9 @@ import numpy as np
 x = np.linspace(0, 1, 100)
 plt.plot(x, np.sin(x), 'b', label='sin')
 plt.plot(x, np.cos(x), 'r', label='cos')
+
+plt.xlabel('t / ns')
+plt.ylabel('U / V')
+
 plt.legend()
 plt.savefig('test.pdf')
