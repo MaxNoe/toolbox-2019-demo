448da6240160ff5bf557851bff428eb6d3b1acfd
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-09-27 13:34:30 +0200

    Add cos, label and legend to example script

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 1, 100)
-plt.plot(x, np.sin(x))
+plt.plot(x, np.sin(x), label='sin')
+plt.plot(x, np.cos(x), label='cos')
+plt.legend()
 plt.savefig('test.pdf')
