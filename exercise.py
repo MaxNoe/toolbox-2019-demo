2d91381b12b1558c94ac401aacedc6bba77e8de6
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-09-27 14:10:05 +0200

    Make sin red, cos blue

--- exercise.py
+++ exercise.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 1, 100)
-plt.plot(x, np.sin(x), label='sin')
-plt.plot(x, np.cos(x), label='cos')
+plt.plot(x, np.sin(x), 'r', label='sin')
+plt.plot(x, np.cos(x), 'b', label='cos')
 plt.legend()
 plt.savefig('test.pdf')
